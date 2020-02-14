931c8800aaf9730206ff2ce2209bfacdb5b5f7c8
orjon <github@orjon.com>
2020-02-14 13:04:15 +0000

    Screenshot updated

--- src/Coin.js
+++ src/Coin.js
@@ -6,7 +6,7 @@ class Coin extends Component {
   render(){
     return(
       <div className='Coin'>
-        {(this.props.side === 'both') && <img src={require('./images/coin-both.png')} alt=''/>}
+        {(this.props.side === 'both') && <img src={require('./coin-both.png')} alt=''/>}
         {(this.props.side === 'heads') && <img src={require('./coin-heads.png')} alt=''/>}
         {(this.props.side === 'tails') && <img src={require('./coin-tails.png')} alt=''/>}     
       </div>
